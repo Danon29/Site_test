e9b0259dfc736a4b04e95a04f88e4a70f802370b
Усманов Руслан <usmanovruslan83@gmail.com>
2022-02-22 17:29:41 +0200

    well

--- Site/Program.cs
+++ Site/Program.cs
@@ -1,9 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Site.Data;
+using Site.Models;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SiteContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SiteContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
